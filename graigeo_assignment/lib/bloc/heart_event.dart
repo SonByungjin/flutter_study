59842a4f8e7a730efd93fb227c770b69f333765d
손병진 <sgyos000@gmail.com>
2021-06-02 19:13:30 +0900

    Fix: remove equatable

--- graigeo_assignment/lib/bloc/heart_event.dart
+++ graigeo_assignment/lib/bloc/heart_event.dart
@@ -1,28 +1,15 @@
-import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
+abstract class HeartEvent{}
 
-@immutable
-abstract class HeartEvent extends Equatable {}
-
-class ListHeartEvent extends HeartEvent {
-  @override
-  List<String> get props => [];
-}
+class ListHeartEvent extends HeartEvent {}
 
 class CreateHeartEvent extends HeartEvent {
   final String item;
 
   CreateHeartEvent(this.item);
-
-  @override
-  List<Object> get props => [this.item];
 }
 
 class DeleteHeartEvent extends HeartEvent {
   final String item;
 
   DeleteHeartEvent(this.item);
-
-  @override
-  List<Object> get props => [this.item];
 }
--- graigeo_assignment/lib/bloc/heart_state.dart
+++ graigeo_assignment/lib/bloc/heart_state.dart
@@ -1,33 +1,17 @@
-import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
+abstract class HeartState {}
 
-@immutable
-abstract class HeartState extends Equatable {}
+class Empty extends HeartState {}
 
-class Empty extends HeartState {
-  @override
-  List<Object> get props => [];
-}
-
-class Loading extends HeartState {
-  @override
-  List<Object> get props => [];
-}
+class Loading extends HeartState {}
 
 class Error extends HeartState {
   final String message;
 
   Error(this.message);
-
-  @override
-  List<Object> get props => [this.message];
 }
 
 class Loaded extends HeartState {
   final List<String> hearts;
 
   Loaded(this.hearts);
-
-  @override
-  List<String> get props => this.hearts;
 }
