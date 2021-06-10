18496c4d9c66cb6439eb288bae381e8abb6be2e4
손병진 <sgyos000@gmail.com>
2021-06-10 20:22:36 +0900

    Fix: landing screen scaffold

--- bitinsider_structure/lib/screens/home/home_screen.dart
+++ bitinsider_structure/lib/screens/home/home_screen.dart
@@ -9,24 +9,50 @@ class LandingScreen extends StatefulWidget {
 }
 
 class _LandingScreenState extends State<LandingScreen> {
+  List<IconData> homeIcons = [
+    Icons.bar_chart,
+    Icons.message,
+    Icons.home_filled,
+    Icons.star,
+    Icons.local_post_office
+  ];
+
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: DefaultTabController(
-        length: 3,
+        length: 5,
+        initialIndex: 2,
         child: Builder(
           builder: (BuildContext context) {
             return Scaffold(
-              appBar: GlobalAppBar(['공지사항', '인기글', '뉴스'], 'BITINSIDER'),
+              appBar: AppBar(
+                title: Text('BITINSIDER'),
+              ),
               body: TabBarView(
                 children: [
-                  Center(child: Text('공지사항')),
+                  Center(child: Text('실시간 시세')),
+                  Center(child: Text('메시지')),
+                  Center(child: Text('홈')),
                   Center(child: Text('인기글')),
-                  Center(child: Text('뉴스')),
+                  Center(child: Text('공지')),
                 ],
               ),
-              bottomNavigationBar: GlobalBottomNav(2),
+              bottomNavigationBar: TabBar(
+                indicatorColor: Colors.transparent,
+                labelColor: Colors.red,
+                unselectedLabelColor: Colors.grey,
+                tabs: homeIcons
+                    .map(
+                      (el) => Tab(
+                        icon: Icon(
+                          el,
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
               drawer: Drawer(
                 child: GlobalDrawer(),
               ),
